907738591243a10eea68bcbcb9cf85922d5e430d
Pavel Lobtsov <plobtsov@gmail.com>
2022-06-13 12:41:18 +0300

    update

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -33,6 +33,7 @@ export const getCurrentUser = createAsyncThunk(
       const { auth } = getState();
       const { token } = auth;
       const user = await services.getCurrent(token);
+      console.log(user);
       return user;
     } catch (error) {
       rejectWithValue(error);
